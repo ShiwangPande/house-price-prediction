278a6391c6107300b3d497fe8ed41b1f7e59ac1f
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-04-09 21:02:52 +0530

    don some changes

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+data.isna().sum
+
+# data.drop(col=['area_type', 'avaibility', 'society', 'balcony'], inplace=True)
+
+# data.describe()
+
+# data.info()
+
+# data['location'].value_counts()
+
+# data['location'] = data['location'].fillna()
+
+# data['size'].value_counts()
+
+# data['size'] = data['size'].fillna()
+
+# data['total_sqft'].value_counts()
+
+# data['total_sqft'] = data['total_sqft'].fillna()
